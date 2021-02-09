4435c9da45334e44c80a0c46ba3bdfd6098c5abd
WK508 <wkno1@naver.com>
2021-02-09 16:37:57 +0900

    2021 02 09
    2021 02 09

---
+++ 20210209/20210209/20210209/CS2006.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210209
+{
+    class CS2006
+    {
+        public const string CS = "2006 ALCS 진출팀";
+
+        private string team;
+        private int year1;
+        private string year2;
+        private string division;
+
+        public CS2006()
+        {
+            team = "DETROIT TIGERS";
+            year1 = 1894;
+            year2 = "1935, 1945, 1968, 1984";
+            division = "아메리칸리그 중부지구 1위";
+        }
+
+        public CS2006(string team, int year1, string year2, string division)
+        {
+            this.team = team;
+            this.year1 = year1;
+            this.year2 = year2;
+            this.division = division;
+        }
+
+        public void showCS2006Info()
+        {
+            Console.WriteLine("팀명 : " + team);
+            Console.WriteLine("창단년도 : " + year1);
+            Console.WriteLine("우승년도 : " + year2);
+            Console.WriteLine("소속 리그/지구/순위 : " + division);
+            Console.WriteLine("---------------------------------------------------------------------------");
+        }
+    }
+
+    
+}
---
+++ 20210209/20210209/20210209/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CS2006 c = new CS2006();
+            c.showCS2006Info();
+
+            CS2006 c2 = new CS2006("OAKLAND ATHLETICS", 1901, "1910, 1911, 1913, 1929, 1930, 1972, 1973, 1974, 1989", "아메리칸리그 서부지구 1위");
+            c2.showCS2006Info();
+        }
+    }
+}
