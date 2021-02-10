1cb346d50d295f8808bcb7a5c75f4ed0cbcc97fb
WK508 <wkno1@naver.com>
2021-02-10 15:47:16 +0900

    2021 02 10
    2021 02 10

---
+++ 20210210/20210210/20210210/DS2020.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210210
+{
+    class DS2020
+    {
+        public const string DS = "내셔널리그 중부지구 팀";
+
+        private string team;
+        private int year1;
+        private string year2;
+        private string division;
+
+        public DS2020()
+        {
+            team = "시카고 컵스";
+            year1 = 1876;
+            year2 = "1907, 1908, 2016";
+            division = "내셔널리그 중부지구 1위";
+        }
+
+        public DS2020(string team, int year1, string year2, string division)
+        {
+            this.team = team;
+            this.year1 = year1;
+            this.year2 = year2;
+            this.division = division;
+        }
+
+        public void showDS2020Info()
+        {
+            Console.WriteLine("팀명 : " + team);
+            Console.WriteLine("창단년도 : " + year1);
+            Console.WriteLine("우승년도 : " + year2);
+            Console.WriteLine("소속 리그/지구/순위 : " + division);
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+        }
+    }
+
+}
---
+++ 20210210/20210210/20210210/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DS2020 d = new DS2020();
+            d.showDS2020Info();
+
+            DS2020 d2 = new DS2020("세인트루이스 카디널스", 1882, "1926, 1931, 1934, 1942, 1944, 1946, 1964, 1967, 1982, 2006, 2011", "내셔널리그 중부지구 2위");
+            d2.showDS2020Info();
+
+            DS2020 d3 = new DS2020("신시내티 레즈", 1882, "1919, 1940, 1975, 1976, 1990", "내셔널리그 중부지구 3위");
+            d3.showDS2020Info();
+
+            DS2020 d4 = new DS2020("밀워키 브루어스", 1969, "없음", "내셔널리그 중부지구 4위");
+            d4.showDS2020Info();
+
+            DS2020 d5 = new DS2020("피츠버그 파이어리츠", 1882, "1909, 1925, 1960, 1971, 1979", "내셔널리그 중부지구 5위");
+            d5.showDS2020Info();
+        }
+    }
+}
